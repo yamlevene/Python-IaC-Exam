77ce72b655e1d9cb0bdca1b2c6b32e15750f194b
Your Name <you@example.com>
2025-02-21 11:17:25 +0000

    allowed existing by user and fixed avz presentation

--- user_input.py
+++ user_input.py
@@ -1,9 +1,17 @@
 from data import UBUNTU_AMI, AMAZON_LINUX_AMI
+import sys
 
+EXIT_CODE = "0"
 UBUNTU, AMAZON_LINUX = "1", "2"
 T3_SMALL, T3_MEDIUM = "1", "2"
 AV_ZONES = {"us-east-1a": "us-east-1"}
 DEFAULT_AV_ZONE = "us-east-1a"
+
+
+def suggest_exit():
+    exit_input = input("do you want to exit? if so - press 0 and click enter, otherwise press any key ")
+    if exit_input == EXIT_CODE:
+        sys.exit(0)
 
 
 def request_ami():
@@ -16,6 +24,7 @@ def request_ami():
             ami = AMAZON_LINUX_AMI
         else:
             print("you must choose 1 or 2 my dear friend")
+            suggest_exit()
     return ami
 
 
@@ -29,6 +38,7 @@ def request_instance_type():
             instance_type = "t3.medium"
         else:
             print("you must choose 1 or 2 my dear friend")
+            suggest_exit()
     return instance_type
 
 
@@ -39,7 +49,7 @@ def request_availability_zone():
         av_zone = user_choice
         return region, av_zone
     except KeyError as k_err:
-        print(f"Sorry! we only support this availability zones at the moment:\n{AV_ZONES.keys()}")
+        print(f"Sorry! we only support this availability zones at the moment:\n{list(AV_ZONES.keys())}")
         print(f"We'll forward you by default to {DEFAULT_AV_ZONE}")
         region = AV_ZONES[DEFAULT_AV_ZONE]
         av_zone = DEFAULT_AV_ZONE
@@ -47,13 +57,13 @@ def request_availability_zone():
 
 
 def request_alb_name():
-    # todo: validation checks other than empty input?
     user_alb_name = None
     valid = False
     while not valid:
-        user_choice = input("Name you ALB please: ")
+        user_choice = input("Name your ALB please: ")
         if user_choice == "":
             print("I can't work with an empty name!")
+            suggest_exit()
         else:
             user_alb_name = user_choice
             valid = True
